ee606f1704796a520d4afbac4729e338063d5f08
Emilio Vicari <emilio.vicari@gmail.com>
2017-02-13 20:32:57 +0100

    added new functionalities

--- core/dci.hpp
+++ core/dci.hpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <cstring>
 #include <stdexcept>
+#include <utility>
 
 using namespace std;
 
@@ -49,16 +51,16 @@ namespace dci
         size_t M; // number of samples
         size_t S; // number of registers per sample
         size_t NB; // number of bits per sample
-        size_t bytes_per_sample;
+        size_t sample_bytes;
         size_t data_bytes;
         size_t agent_pool_bytes;
         inline void init(const size_t& N, const size_t& M, const size_t& NB)
         {
             this->N = N; this->M = M; this->NB = NB;
             S = register_utils::number_of_registers(NB);
-            bytes_per_sample = S * bytes_per_reg;
-            data_bytes = bytes_per_sample * M;
-            agent_pool_bytes = bytes_per_sample * N;
+            sample_bytes = S * bytes_per_reg;
+            data_bytes = sample_bytes * M;
+            agent_pool_bytes = sample_bytes * N;
         }
     };
 
@@ -76,6 +78,9 @@ namespace dci
         agent() : bitmask(nullptr), name() { }
         agent(reg_type* agent_bitmask, const string& agent_name, system* parent_system) : bitmask(agent_bitmask), name(agent_name), parent(parent_system) { }
     public:
+
+        string get_name() { return name; }
+        const string& get_name() const { return name; }
 
         // friend classes
         friend class system;
@@ -145,52 +150,109 @@ namespace dci
         reg_type* data;
         reg_type* agent_pool;
         system_properties props;
+        inline void reset_fields()
+        {
+            samples.resize(0);
+            agents.resize(0);
+            data = nullptr;
+            agent_pool = nullptr;
+            props.init(0, 0, 0);            
+        }
         void free_data(const bool& reset = false)
         {
             for (auto& s : samples) if (s) delete s; for (auto& a : agents) if (a) delete a; if (data) delete[] data; if (agent_pool) delete[] agent_pool;
-            if (reset)
-            {
-                samples.resize(0);
-                agents.resize(0);
-                data = nullptr;
-                agent_pool = nullptr;
-                props.init(0, 0, 0);
-            }
+            if (reset) reset_fields();
         }
-        void allocate_data() 
+        void allocate_data(const size_t& N, const size_t& M, const size_t& NB) 
         {
+            props.init(N, M, NB);
             samples.resize(props.M, nullptr);
             agents.resize(props.N, nullptr);
+            
+            // empty system
+            if (!props.N)
+            {
+                data = nullptr;
+                agent_pool = nullptr;
+                return;
+            }
+
             data = new reg_type[props.data_bytes];
             agent_pool = new reg_type[props.agent_pool_bytes];
 
             // reset memory
             memset(data, 0, props.data_bytes);
-            memset(agent_pool, 0, props.agent_pool_bytes);           
+            memset(agent_pool, 0, props.agent_pool_bytes);
+
+            // set internal pointers for agents and samples
+            for (size_t a = 0; a != props.N; ++a)
+                agents[a] = new agent(agent_pool + a * props.S, string(1, '0' + a), this);
+            for (size_t s = 0; s != props.M; ++s)
+                samples[s] = new sample(data + s * props.S, this);
+        }
+        void copy_from(const system& s)
+        {
+            allocate_data(s.props.N, s.props.M, s.props.NB);
+
+            // copy raw data and agent pool
+            if (data) memcpy(data, s.data, props.data_bytes);
+            if (agent_pool) memcpy(agent_pool, s.agent_pool, props.agent_pool_bytes);
+        }
+        void move_from(system&& s)
+        {
+            props = s.props;
+            data = s.data;
+            agent_pool = s.agent_pool;
+            samples = move(s.samples);
+            agents = move(s.agents);
+            for (auto& a : agents) a->parent = this;
+            for (auto& a : samples) a->parent = this;
+
+            s.reset_fields();
         }
     public:
 
         system() : samples(0), agents(0), data(nullptr), agent_pool(nullptr) { props.init(0, 0, 0); }
         system(const size_t& N, const size_t& M, const size_t& bits_per_agent = 1)
         {  
-            props.init(N, M, N * bits_per_agent);
-            allocate_data();
+            allocate_data(N, M, N * bits_per_agent);
 
             // build agent pool
             for (size_t a = 0; a != props.N; ++a)
-            {
-                // create agent pointing to corresponding agent pool position
-                reg_type* cur_agent = agent_pool + a * props.S;
-                agents[a] = new agent(cur_agent, string(1, '0' + a), this);
-
                 // set bits in agent bitmask
                 for (size_t b = 0; b != bits_per_agent; ++b)
-                    register_utils::set_bit(cur_agent, b + a * bits_per_agent, 1);
-            }
+                    register_utils::set_bit(agent_pool + a * props.S, b + a * bits_per_agent, 1);
 
-            // build sample pool
-            for (size_t s = 0; s != props.M; ++s)
-                samples[s] = new sample(data + s * props.S, this);
+        }
+
+        // copy constructor/assignment
+        system(const system& s) 
+        {
+            copy_from(s);
+        }
+        const system& operator=(const system& s)
+        {
+            if (&s != this)
+            {
+                free_data();
+                copy_from(s);
+            }
+            return *this;
+        }
+
+        // move constructor/assignment
+        system(system&& s) 
+        {
+            move_from(move(s));
+        }
+        const system& operator=(system&& s)
+        {
+            if (&s != this)
+            {
+                free_data();
+                move_from(move(s));
+            }
+            return *this;
         }
 
         // number of samples, agents
@@ -228,6 +290,28 @@ namespace dci
         friend bool operator==(const system& s1, const system& s2);
         friend bool operator!=(const system& s1, const system& s2);
         friend istream& operator>>(istream& in, system& s);
+
+        // save functions
+        void save_to_file(const string& file_path)
+        {
+            ofstream out(file_path);
+            out << *this;
+        }
+
+        // homogeneous system generation
+        system generate_homogeneous_system()
+        {
+
+
+            system s;
+            s.allocate_data(props.N, props.M, props.NB);
+
+            // just copy agent pool
+            if (agent_pool) memcpy(s.agent_pool, agent_pool, props.agent_pool_bytes);
+
+            return s;
+
+        }
 
         virtual ~system() { free_data(); }
     };
@@ -336,8 +420,7 @@ namespace dci
         NB = lines[offset].size();
         N = offset ? (offset - 2) : NB;
         M = lines.size() - offset;
-        s.props.init(N, M, NB);
-        s.allocate_data();
+        s.allocate_data(N, M, NB);
 
         // build agent pool
         for (size_t a = 0; a != s.props.N; ++a)
@@ -347,17 +430,33 @@ namespace dci
             s.agents[a] = new agent(cur_agent, string(1, '0' + a), &s);
 
             // set bits in agent bitmask
-            register_utils::set_bit(cur_agent, a, 1);
+            if (offset)
+                register_utils::set_bits_from_string((s.agents[a])->bitmask, lines[a + 1]);
+            else
+                register_utils::set_bit(cur_agent, a, 1);
         }
 
         // build sample pool
         for (size_t i = 0; i != s.props.M; ++i)
         {
             s.samples[i] = new sample(s.data + i * s.props.S, &s);
-            register_utils::set_bits_from_string((s.samples[i])->data, lines[i]);
+            register_utils::set_bits_from_string((s.samples[i])->data, lines[i + offset]);
         }
 
         return in;
+    }
+
+    //
+    // System factories
+    //
+
+    // load from file
+    system load_from_file(const string& file_path)
+    {
+        ifstream in(file_path);
+        system s;
+        in >> s;
+        return s;
     }
 
 }
--- test/dci_test.cpp
+++ test/dci_test.cpp
@@ -95,13 +95,8 @@ TEST_CASE( "System save/load", "[system]" ) {
     for (size_t s = 0; s != d.num_samples(); ++s)
         for (size_t a = 0; a != d.num_agents(); ++a)
             d[s][a] = (a+s)%2;
-    ofstream fout("test.txt");
-    fout << d;
-    fout.close();
-    dci::system e;
-    ifstream fin("test.txt");
-    fin >> e;
-    fin.close();
+    d.save_to_file("test.txt");
+    dci::system e = dci::load_from_file("test.txt");
     REQUIRE( d == e );
 }
 
@@ -110,12 +105,89 @@ TEST_CASE( "System save/load - multilevel", "[system]" ) {
     for (size_t s = 0; s != d.num_samples(); ++s)
         for (size_t a = 0; a != d.num_agents(); ++a)
             d[s][a] = (a+s)%16;
-    ofstream fout("test.txt");
-    fout << d;
-    fout.close();
-    dci::system e;
-    ifstream fin("test.txt");
-    fin >> e;
-    fin.close();
+    d.save_to_file("test.txt");
+    dci::system e = dci::load_from_file("test.txt");
     REQUIRE( d == e );
+}
+
+TEST_CASE( "Copy constructor and assignment", "[system]") {
+    dci::system d(10, 100);
+    for (size_t s = 0; s != d.num_samples(); ++s)
+        for (size_t a = 0; a != d.num_agents(); ++a)
+            d[s][a] = 1U;
+    dci::system e(10, 100);
+    for (size_t s = 0; s != d.num_samples(); ++s)
+        for (size_t a = 0; a != d.num_agents(); ++a)
+            e[s][a] = 0U;
+    dci::system f = d;
+    REQUIRE( d == f );
+    f = e;
+    REQUIRE( f == e );
+    for (size_t s = 0; s != d.num_samples(); ++s)
+        for (size_t a = 0; a != d.num_agents(); ++a)
+            f[s][a] = 1U;
+    REQUIRE( f == d );
+    REQUIRE( f != e );
+}
+
+TEST_CASE( "Copy constructor and assignment - multilevel", "[system]") {
+    dci::system d(10, 100, 4);
+    for (size_t s = 0; s != d.num_samples(); ++s)
+        for (size_t a = 0; a != d.num_agents(); ++a)
+            d[s][a] = 15U;
+    dci::system e(10, 100, 4);
+    for (size_t s = 0; s != d.num_samples(); ++s)
+        for (size_t a = 0; a != d.num_agents(); ++a)
+            e[s][a] = 0U;
+    dci::system f(d);
+    REQUIRE( d == f );
+    f = e;
+    REQUIRE( f == e );
+    for (size_t s = 0; s != d.num_samples(); ++s)
+        for (size_t a = 0; a != d.num_agents(); ++a)
+            f[s][a] = 15U;
+    REQUIRE( f == d );
+    REQUIRE( f != e );
+}
+
+TEST_CASE( "Move constructor and assignment", "[system]") {
+    dci::system empty;
+    dci::system d;
+    REQUIRE( d == empty );
+    d = dci::system(10, 100);
+    REQUIRE( d.num_agents() == 10 );
+    REQUIRE( d.num_samples() == 100 );
+    for (size_t s = 0; s != d.num_samples(); ++s)
+        for (size_t a = 0; a != d.num_agents(); ++a)
+            d[s][a] = 1U;
+    
+    dci::system e = d;
+    
+    dci::system f = move(d);
+    for (size_t s = 0; s != e.num_samples(); ++s)
+        for (size_t a = 0; a != e.num_agents(); ++a)
+            REQUIRE( e[s][a] == 1U );    
+    REQUIRE( e == f );
+    REQUIRE( d == empty );
+}
+
+TEST_CASE( "Move constructor and assignment - multilevel", "[system]") {
+    dci::system empty;
+    dci::system d;
+    REQUIRE( d == empty );
+    d = dci::system(10, 100, 4);
+    REQUIRE( d.num_agents() == 10 );
+    REQUIRE( d.num_samples() == 100 );
+    for (size_t s = 0; s != d.num_samples(); ++s)
+        for (size_t a = 0; a != d.num_agents(); ++a)
+            d[s][a] = 15U;
+    
+    dci::system e = d;
+    
+    dci::system f = move(d);
+    for (size_t s = 0; s != e.num_samples(); ++s)
+        for (size_t a = 0; a != e.num_agents(); ++a)
+            REQUIRE( e[s][a] == 15U );    
+    REQUIRE( e == f );
+    REQUIRE( d == empty );
 }
